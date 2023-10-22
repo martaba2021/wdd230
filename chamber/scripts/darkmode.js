362b2991a8419f725633529651c2fe8a564e59ea
marta.rod <rod21062@byui.edu>
2023-10-21 22:56:35 -0400

    Week04NewAtt

--- chamber/scripts/darkmode.js
+++ chamber/scripts/darkmode.js
@@ -1,5 +1,26 @@
-const mode = document.getElementById('mode');
+const darkMode = document.getElementById('mode');
+const body = document.body;
 
-mode.addEventListener('click', () => {
-    document.body.classList.toggle('dark');
+darkMode.addEventListener('change', () => {
+  if (darkMode.checked) {
+    body.classList.add('theme-dark');
+  } else {
+    body.classList.remove('theme-dark');
+  }
 });
+function toggleDarkMode() {
+const darkThemeLink = document.getElementById('dark-theme-link');
+  if (darkThemeLink) {
+    // Dark mode CSS is already loaded, so remove it to switch back to the light theme.
+    darkThemeLink.parentNode.removeChild(darkThemeLink);
+} else {
+    // Dark mode CSS is not loaded, so create a new link element and add it to the head.
+    const link = document.createElement('link');
+    link.id = 'dark-theme-link';
+    link.rel = 'stylesheet';
+    link.href = 'styles/dark-theme.css'; 
+    document.head.appendChild(link);
+}}
+const darkModeButton = document.getElementById('dark-mode-button'); 
+darkModeButton.addEventListener('click', toggleDarkMode);
+  
