89b2fb9cd98111236820b830dbf9b307b80ce5b1
marta.rod <rod21062@byui.edu>
2023-11-04 19:54:10 -0400

    timestamp

---
+++ chamber/scripts/timestamp.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function () {
+    var timestampElement = document.getElementById("timestamp")
+    var currentTime = new Date();
+    var timestamp = currentTime.toISOString
+    timestampElement.textContent = timestamp;
+});
