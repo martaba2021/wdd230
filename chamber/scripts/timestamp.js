be77988a9a7b464b9426dec0791c112d9d24f16c
marta.rod <rod21062@byui.edu>
2023-11-04 20:26:33 -0400

    joinStyle

--- chamber/scripts/timestamp.js
+++ chamber/scripts/timestamp.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", function () {
-    var timestampElement = document.getElementById("timestamp");
+    var timestampInput= document.getElementById("timestamp");
     var currentTime = new Date();
-    var timestamp = currentTime.toISOString();
-    
-    timestampElement.textContent = timestamp;
+    var timestamp = currentTime.toISOString().slice().replace("T", " ");
+
+    timestampInput.textContent = timestamp;
 });
