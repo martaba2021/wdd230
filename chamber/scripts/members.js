1aa987d3d811fa32d625f6ddbeab9325c6db7968
marta.rod <rod21062@byui.edu>
2023-11-11 22:50:21 -0500

    week09Corrections

--- chamber/scripts/members.js
+++ chamber/scripts/members.js
@@ -4,45 +4,57 @@ const membersURL = "https://martaba2021.github.io/wdd230/chamber/data/members.js
 const directoryList = document.querySelector('#members');
 
 async function getMembers() {
-    try {
         const response = await fetch(membersURL);
         const data = await response.json();
+        // console.log(data);
         displayMembers(data.members);
-    } catch (error) {
-        console.error('Error fetching members:', error);
-    }
 }
 
-function displayMembers(memberships) {
-    const membersList = document.getElementById('members');
+function displayMembers(companies) {
+    companies.forEach((member) => {
+        console.log(member);
+        let section = document.createElement('section');
+        let name = document.createElement('h3');
+        let address = document.createElement('p');
+        let phoneNumber = document.createElement('p');
+        let companyURL = document.createElement('a');
+        let image = document.createElement('img');
+        let membershipLevel = document.createElement('p');
+        let yearsInBusiness = document.createElement('p');
+        
+        name.textContent = `Company: ${member.name}`;
+        address.textContent = `Address: ${member.address}`;
+        phoneNumber.textContent = `Phone: ${member.phoneNumber}`;
+        companyURL.textContent = `${member.companyURL}`;
+        companyURL.setAttribute('href', member.companyURL);
+        companyURL.setAttribute('target', "_blank");
+        image.setAttribute('src', member.image);
+        image.setAttribute('alt', member.name);
+        membershipLevel.textContent = `Membership Level: ${member.membershipLevel}`;
+        yearsInBusiness.textContent = `Years in Business: ${member.yearsInBusiness}`;
 
-    memberships.forEach((member) => {
-        let listItem = document.createElement('li');
 
-        // Display member name
-        let nameElement = document.createElement('h3');
-        nameElement.textContent = member.name;
-        listItem.appendChild(nameElement);
+        section.appendChild(image);
+        section.appendChild(name);
+        section.appendChild(address);
+        section.appendChild(phoneNumber);
+        section.appendChild(membershipLevel);
+        section.appendChild(yearsInBusiness);
+        section.appendChild(companyURL)
 
-        // Create a sub-list for member details
-        let detailsList = document.createElement('ul');
-        detailsList.innerHTML = `
-            <li><strong>Address:</strong> ${member.address}</li>
-            <li><strong>Phone Number:</strong> ${member.phoneNumber}</li>
-            <li><strong>Website:</strong> <a href="${chamberURL + member.websiteURL}" target="_blank">${member.websiteURL}</a></li>
-            <li><strong>Membership Level:</strong> ${member.membershipLevel}</li>
-            <li><strong>Years in Business:</strong> ${member.yearsInBusiness}</li>
-        `;
-
-        listItem.appendChild(detailsList);
-
-        // Display member image
-        let imageElement = document.createElement('img');
-        imageElement.setAttribute('src', chamberURL + member.image);
-        imageElement.setAttribute('alt', `${member.name} Image`);
-        listItem.appendChild(imageElement);
-
-        membersList.appendChild(listItem);
+        directoryList.appendChild(section);
     });
+    
 }
-getMembers()
+
+gridButton.addEventListener('click', () => {
+    directory.classList.remove('list-view');
+    directory.classList.add('grid-view');
+});
+
+listButton.addEventListener('click', () => {
+    directory.classList.remove('grid-view');
+    directory.classList.add('list-view');
+});
+
+getMembers();
