79dfecf352c3d58270a56df066519c766a1d870c
marta.rod <rod21062@byui.edu>
2023-12-02 12:23:33 -0500

    final fixes

---
+++ final/scripts/prices.js
@@ -0,0 +1,65 @@
+const homeURL = "https://martaba2021.github.io/wdd230/final/";
+const pricesURL = "https://martaba2021.github.io/wdd230/final/data/prices.json";
+const directory = document.querySelector('.grid-view');
+
+async function getPrices()) {
+        const response = await fetch(pricesURL);
+        const data = await response.json();
+        // console.log(data);
+        displayMembers(data.Max Rental Pricing);
+}
+
+function displayMembers(members) {
+    members.forEach((member) => {
+        //console.log(member);
+        let section = document.createElement('section');
+        let name = document.createElement('h3');
+        let address = document.createElement('p');
+        let phoneNumber = document.createElement('p');
+        let websiteURL = document.createElement('a');
+        let image = document.createElement('img');
+        let membershipLevel = document.createElement('p');
+        let yearsInBusiness = document.createElement('p');
+
+        section.classList.add("card");
+        
+        name.textContent = `${member.name}`;
+        address.textContent = `Address: ${member.address}`;
+        phoneNumber.textContent = `Phone: ${member.phoneNumber}`;
+        websiteURL.textContent = `${member.websiteURL}`;
+        websiteURL.setAttribute('href', member.websiteURL);
+        websiteURL.setAttribute('target', "_blank");
+        image.setAttribute('src', member.image);
+        image.setAttribute('alt', member.name);
+        membershipLevel.textContent = `Membership Level: ${member.membershipLevel}`;
+        yearsInBusiness.textContent = `Years in Business: ${member.yearsInBusiness}`;
+
+        section.appendChild(image);
+        section.appendChild(name);
+        section.appendChild(address);
+        section.appendChild(phoneNumber);
+        section.appendChild(membershipLevel);
+        section.appendChild(yearsInBusiness);
+        section.appendChild(websiteURL)
+
+        directory.appendChild(section);
+    });
+    
+}
+getMembers();
+
+document.addEventListener('DOMContentLoaded', () => {
+const gridButton = document.getElementById('gridButton');
+const listButton = document.getElementById('listButton');
+const display = document.querySelector('article')
+
+gridButton.addEventListener('click', () => {
+    display.classList.remove('list-view');
+    display.classList.add('grid-view');
+});
+
+listButton.addEventListener('click', () => {
+    display.classList.remove('grid-view');
+    display.classList.add('list-view');
+});
+})
