7c0ca2ee65748a9396517624b509594973200e85
marta.rod <rod21062@byui.edu>
2023-12-02 09:16:34 -0500

    menu fixes

--- final/scripts/toggles-menu.js
+++ final/scripts/toggles-menu.js
@@ -4,4 +4,5 @@ const navigation = document.querySelector('.navigation');
 hamButton.addEventListener('click', () => {
 	navigation.classList.toggle('open');
 	hamButton.classList.toggle('open');
-});
+}
+);
