15c9e9cb2bcd8235c8cf3a72e05f30efdc4bcb85
marta.rod <rod21062@byui.edu>
2023-12-02 16:15:16 -0500

    rentals updates

--- final/scripts/prices.js
+++ final/scripts/prices.js
@@ -1,65 +1,57 @@
 const homeURL = "https://martaba2021.github.io/wdd230/final/";
 const pricesURL = "https://martaba2021.github.io/wdd230/final/data/prices.json";
-const directory = document.querySelector('.grid-view');
+const tableContainer = document.querySelector('.price-table');
 
 async function getPrices() {
-        const response = await fetch(pricesURL);
-        const data = await response.json();
-        // console.log(data);
-        displayPrices(data.maxRentalPricing);
+    const response = await fetch(pricesURL);
+    const data = await response.json();
+    //console.log(data);
+
+    displayPrices(data.maxRentalPricing);
 }
+function displayPrices(rentalPrices) {
+    // Create a table element
+    const table = document.createElement('table');
+    table.classList.add('price-table');
+    //console.log(rentalPrices);
 
-function displayPrices(maxRentalPricing) {
-    maxRentalPricing.forEach((price) => {
-        //console.log(member);
-        let section = document.createElement('section');
-        let rentalType = document.createElement('h3');
-        let maxPerson = document.createElement('p');
-        let reservation = document.createElement('h4');
-        let halfDay = document.createElement('p');
-        let fullDay = document.createElement('p');
-        let walkin = document.createElement('h4');
-        let halfDay = document.createElement('p');
+    // Add a table caption (name)
+    const caption = table.createCaption();
+    caption.textContent = 'Max Rental Pricing';
 
-        section.classList.add("card");
+    // Create the table header
+    const headerRow = table.insertRow();
+    const headerCell1 = headerRow.insertCell(0);
+    const headerCell2 = headerRow.insertCell(1);
+    const headerCell3 = headerRow.insertCell(2);
+    const headerCell4 = headerRow.insertCell(3);
+
+    headerCell1.textContent = 'Rental Type';
+    headerCell2.textContent = 'Max Persons';
+    headerCell3.textContent = 'Advance Reservation';
+    headerCell4.textContent = 'Walk-in';
+
+    rentalPrices.forEach((price) => {
+        const row = table.insertRow();
+        const cell1 = row.insertCell(0);
+        const cell2 = row.insertCell(1);
+        const cell3 = row.insertCell(2);
+        const cell4 = row.insertCell(3);
+
+        cell1.textContent = price.rentalType;
+        cell2.textContent = price.maxPerson;
         
-        name.textContent = `${member.name}`;
-        address.textContent = `Address: ${member.address}`;
-        phoneNumber.textContent = `Phone: ${member.phoneNumber}`;
-        websiteURL.textContent = `${member.websiteURL}`;
-        websiteURL.setAttribute('href', member.websiteURL);
-        websiteURL.setAttribute('target', "_blank");
-        image.setAttribute('src', member.image);
-        image.setAttribute('alt', member.name);
-        membeipLevel.textContent = `Membership Level: ${member.membershipLevel}`;
-        yearsInBusiness.textContent = `Years in Business: ${member.yearsInBusiness}`;
 
-        section.appendChild(image);
-        section.appendChild(name);
-        section.appendChild(address);
-        section.appendChild(phoneNumber);
-        section.appendChild(membershipLevel);
-        section.appendChild(yearsInBusiness);
-        section.appendChild(websiteURL)
-
-        directory.appendChild(section);
+        cell3.textContent = (price.reservation)?
+            `Half day: ${price.reservation[0].halfDay}, Full day: ${price.reservation[0].fullDay}` :'';
+            
+            //console.log('walkin:', price.walkin);
+            //console.log('walkin[0]:', price.walkin && price.walkin[0]);
+            
+        cell4.textContent = (price.walkin)?
+            `Half day: ${price.walkin[0].halfDay}, Full day: ${price.walkin[0].fullDay}` : '';
     });
-    
+
+    tableContainer.appendChild(table);
 }
-getMembers();
-
-document.addEventListener('DOMContentLoaded', () => {
-const gridButton = document.getElementById('gridButton');
-const listButton = document.getElementById('listButton');
-const display = document.querySelector('article')
-
-gridButton.addEventListener('click', () => {
-    display.classList.remove('list-view');
-    display.classList.add('grid-view');
-});
-
-listButton.addEventListener('click', () => {
-    display.classList.remove('grid-view');
-    display.classList.add('list-view');
-});
-})
+getPrices();
