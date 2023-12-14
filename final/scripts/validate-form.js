bfd8c8e5885d27c3ab7a342adafb4bf09c742f7d
marta.rod <rod21062@byui.edu>
2023-12-13 20:09:51 -0500

    reservations done

---
+++ final/scripts/validate-form.js
@@ -0,0 +1,35 @@
+function validateForm() {
+    document.querySelectorAll('.error').forEach(el => el.textContent = '');
+
+    var isValid = true;
+
+    var dateInput = document.getElementById('date');
+    var currentDate = new Date();
+    var selectedDate = new Date(dateInput.value);
+
+    if (selectedDate <= currentDate) {
+        document.getElementById('date-error').textContent = 'Please select a date in the future.';
+        isValid = false;
+    }
+
+    var rentalsInput = document.getElementById('number');
+    var numRentals = parseInt(rentalsInput.value);
+
+    if (numRentals < 1) {
+        document.getElementById('number-error').textContent = 'Number of rentals cannot be negative or 0.';
+        isValid = false;
+    }
+
+    var agreementCheckbox = document.getElementById('agreement');
+    if (!agreementCheckbox.checked) {
+        document.getElementById('agreement-error').textContent = 'Please agree to the rental services and agreements.';
+        isValid = false;
+    }
+
+    if (!isValid) {
+        return false;
+    }
+    return true;
+}
+
+
